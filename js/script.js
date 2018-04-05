2dbf0282b1213bc278fe6c75ebdab71cdde85fc9
Gordislava <gordislava@gmail.com>
2018-04-05 16:22:38 +0300

    JS для всплывающей карты

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ var loginField = popup.querySelector(".input-name");
 var emailField = popup.querySelector(".input-email");
 var commentField = popup.querySelector("[name=comment]");
 var form = popup.querySelector("form");
+var linkMap = document.querySelector(".link-modal-map");
+var popupMap = document.querySelector(".modal-map");
+var closeMap = document.querySelector(".map-close");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -21,6 +24,18 @@ close.addEventListener("click", function (evt) {
 form.addEventListener("submit", function (evt) {
   if (!loginField.value || !emailField.value || !commentField.value) {
   evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
   popup.classList.add("modal-error");
   }
+});
+
+linkMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.add("modal-display");
+});
+
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("modal-display");
 });
