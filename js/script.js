c1fe372b170d44f5321249d53be4e573315a6cf9
Gordislava <gordislava@gmail.com>
2018-04-06 21:25:17 +0300

    Слайдер, мелкие исправления, минификация

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ var form = popup.querySelector("form");
 var linkMap = document.querySelector(".link-modal-map");
 var popupMap = document.querySelector(".modal-map");
 var closeMap = document.querySelector(".map-close");
+var back = document.querySelector(".slider-button-back");
+var next = document.querySelector(".slider-button-next");
+var slide1 = document.querySelector(".slide1");
+var slide2 = document.querySelector(".slide2");
+var delivery = document.querySelector(".service-delivery");
+var warranty = document.querySelector(".service-warranty");
+var credit = document.querySelector(".service-credit");
+var itemDelivery = document.querySelector(".service-text-item-delivery");
+var itemWarranty = document.querySelector(".service-text-item-warranty");
+var itemCredit = document.querySelector(".service-text-item-credit");
+var drills = document.querySelector(".drills");
+var perforators = document.querySelector(".perforators");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -38,4 +50,70 @@ linkMap.addEventListener("click", function (evt) {
 closeMap.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupMap.classList.remove("modal-display");
+});
+
+next.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log(e.target.parentNode);
+  if (slide2.classList.contains("show")) {
+    slide2.classList.remove("show");
+    slide1.classList.add("show");
+    slide1.classList.add("show");
+    perforators.classList.remove("active");
+    drills.classList.add("active");
+  } else {
+    slide1.classList.remove("show");
+    slide2.classList.add("show");
+    drills.classList.remove("active");
+    perforators.classList.add("active");
+  }
+});
+
+back.addEventListener("click", function(e) {
+  console.log(e.target.parentNode);
+  if (slide2.classList.contains("show")) {
+    slide2.classList.remove("show");
+    slide1.classList.add("show");
+    slide1.classList.add("show");
+    perforators.classList.remove("active");
+    drills.classList.add("active");
+  } else {
+    slide1.classList.remove("show");
+    slide2.classList.add("show");
+    drills.classList.remove("active");
+    perforators.classList.add("active");
+  }
+});
+
+delivery.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log(e.target.parentNode);
+  warranty.classList.remove("current-item");
+  credit.classList.remove("current-item");
+  delivery.classList.add("current-item");
+  itemWarranty.classList.remove("active");
+  itemCredit.classList.remove("active");
+  itemDelivery.classList.add("active");
+});
+
+warranty.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log(e.target.parentNode);
+  delivery.classList.remove("current-item");
+  credit.classList.remove("current-item");
+  warranty.classList.add("current-item");
+  itemWarranty.classList.remove("active");
+  itemDelivery.classList.remove("active");
+  itemCredit.classList.add("active");
+});
+
+credit.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log(e.target.parentNode);
+  warranty.classList.remove("current-item");
+  delivery.classList.remove("current-item");
+  credit.classList.add("current-item");
+  itemDelivery.classList.remove("active");
+  itemCredit.classList.remove("active");
+  itemWarranty.classList.add("active");
 });
